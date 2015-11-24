e84fa5c28dd0be21bf8aaa0dfc65f3b30aac8851
Lindani <lindani@projectcodex.co>
2015-11-24 15:56:01 +0200

    Added a function that returns phone calls only for Vodacom

--- itemised_bill.py
+++ itemised_bill.py
@@ -15,3 +15,11 @@ def itemised_bill_list_of_maps(value):
     for values in value:
         dictList.append(dict(zip(key, values)))
     return dictList
+
+
+def itemised_bill_provider_numbers(listOfMaps, provider):
+    call_list = []
+    for network in listOfMaps:
+        if network['Provider'] == provider:
+            call_list.append(network['Number'])
+    return call_list
