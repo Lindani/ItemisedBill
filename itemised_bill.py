b72e4a7516a44c062ed2d533a0550c7015ab86f6
Lindani <lindani@projectcodex.co>
2015-11-24 14:28:36 +0200

    Added new function to have a list of maps from the csv file

--- itemised_bill.py
+++ itemised_bill.py
@@ -6,3 +6,12 @@ def itemised_bill(data):
     with open(data, 'r') as f:
         data = [row for row in csv.reader(f.read().splitlines())]
     return data
+
+
+def itemised_bill_list_of_maps(value):
+
+    key = ['Date', 'Provider', 'Number', 'Duration']
+    dictList = []
+    for values in value:
+        dictList.append(dict(zip(key, values)))
+    return dictList
