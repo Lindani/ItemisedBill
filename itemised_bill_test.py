d469a6869a2d987cf99f5659d2995bd898ad727a
Lindani <lindani@projectcodex.co>
2015-11-25 14:30:59 +0200

    Passing tests for returned duration and and duration in seconds per call

--- itemised_bill_test.py
+++ itemised_bill_test.py
@@ -1,6 +1,8 @@
 from itemised_bill import (itemised_bill,
                            itemised_bill_list_of_maps,
-                           itemised_bill_provider_numbers)
+                           itemised_bill_provider_numbers,
+                           itemised_bill_call_duration,
+                           itemised_bill_duration_in_seconds)
 import unittest
 
 
@@ -10,6 +12,7 @@ class MyTest(unittest.TestCase):
         self.data = 'ItemisedBill.csv'
         self.listofLists = itemised_bill(self.data)
         self.listOfMaps = itemised_bill_list_of_maps(self.listofLists)
+        self.duration = itemised_bill_call_duration(self.listOfMaps)
 
     def test_itemised_bill_data(self):
 
@@ -26,6 +29,17 @@ class MyTest(unittest.TestCase):
     def test_itemised_bill_provider_numbers(self):
         phone_call = itemised_bill_provider_numbers(self.listOfMaps, 'Vodacom')
         self.assertEqual(phone_call[0], '0821302398')
+
+    # def test_itemised_bill_call_numbers_per_provider(self):
+    #     number_of_calls = itemised_bill_call_numbers_per_provider(self.listOfMaps)
+    #     self.assertEqual(number_of_calls, 848484)
+
+    def test_itemised_bill_call_duration(self):
+        self.assertEqual(self.duration, '00h05m34s')
+
+    def test_itemised_bill_duration_in_seconds(self):
+        duration_in_seconds = itemised_bill_duration_in_seconds(self.duration)
+        self.assertEqual(duration_in_seconds, 334)
 
 if __name__ == '__main__':
     unittest.main()
