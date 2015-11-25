908d04fcb70f52baf3236bbf5d31e1dcc562a72e
Lindani <lindani@projectcodex.co>
2015-11-25 14:29:34 +0200

    Added two functions to return duration per call and the other to return duration in seconds per call

--- itemised_bill.py
+++ itemised_bill.py
@@ -1,4 +1,6 @@
 import csv
+import datetime
+import time
 
 
 def itemised_bill(data):
@@ -23,3 +25,33 @@ def itemised_bill_provider_numbers(listOfMaps, provider):
         if network['Provider'] == provider:
             call_list.append(network['Number'])
     return call_list
+
+
+# def itemised_bill_call_numbers_per_provider(listOfMaps):
+#     # myDict = {}
+#     for row in listOfMaps:
+#         # network = row['MTN']
+
+#         # if myDict[network] is None:
+#         #     myDict[network].count(call)
+#         return row
+# def itemised_bill_call_numbers_per_provider(listOfMaps):
+#     call_list = []
+#     for network in listOfMaps:
+#             call_list.append(network['Number'])
+#     myDict = {}
+#     for mylist in call_list:
+#         if myDict[myDict] is None:
+#             myDict[myDict] = 0
+#         myDict += 1
+
+#     return myDict
+def itemised_bill_call_duration(listOfMaps):
+    mylist = []
+    for rows in listOfMaps:
+        mylist.append(rows['Duration'])
+    return mylist[0]
+
+
+def itemised_bill_duration_in_seconds(duration):
+    return int(duration[0:2]) * 3600 + int(duration[3:5]) * 60 + int(duration[6:8])
